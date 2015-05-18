465ebb38de35c25b3826381c19f053a7f1d06d06
Kishor Maharjan <lasataya@gmail.com>
2015-05-19 00:27:48 +1000

    Directive Testing

--- content/script/module/nav.js
+++ content/script/module/nav.js
@@ -1,75 +1,94 @@
 define(["angular"], function(angular){
 	var navApp = angular.module('navApp', []);
-	var status = 0;
 
 	navApp.service('navServices', ['$http', '$q', function(http, q){
-		var deferObject,
+		var deferObject = q.defer(),
 		myMethods = {
-			getNavPromise: function(){
-				var  promise = http.get('JSON/nav.json'),
-					deferObject = deferObject || q.defer();
-
-					promise.then(
-						function(success){
-							deferObject.resolve(success);
-						},
-						function(error){
-							deferObject.resolve(error);
-						});
+			getNavPromise : function(){
+				var promise = http.get('JSON/nav.json');
+				
+				promise.then(
+					function(success){
+						deferObject.resolve(success);
+					},
+					function(error){
+						deferObject.resolve(error);
+					});
 				return deferObject.promise;
-			},
-			getNotificationPromise: function(){
-				var promise = http.get('partials/notifaction.html'),
-					deferObject = deferObject || q.defer();
+			}
 
-					promise.then(
-						function(success){
-							deferObject.resolve(success);
-						},
-						function(error){
-							deferObject.resolve(error);
-						});
+			/*getNotification : function(){
+				var promise = http.get('partials/notification.html');
+
+				promise.then(
+					function(success){
+						deferObject.resolve(success);
+					},
+					function(error){
+						deferObject.resolve(error);
+					});
 				return deferObject.promise;
-			};
+			}*/
 		};
-
-		/*http.get('JSON/nav.json').then(function(data){
-			deffered.resolve(data);
-		});
-
-		this.getMenu = function(){
-			deferObject = deferObject || q.defer(); //we can use one variable "deffered" instead of instantiating multipe $q.defer() objects
-			return deffered.promise;
-		};
-
-		this.setTime = function(){
-				deferObject = deferObject|| q.defer();
-				return deffered.promise;
-		};*/
+		return myMethods;
 
 	}])
+	/*.directive('slideContent', function(){
+		return{
+			transclude: "true",
+			controller: function($scope, $compile){
+				//$scope.words = [];
+				
+				$scope.addClass = function(className){
+					if(!elem.hasClass(element, className)){
+							elem.addClass(className);
+						}
+						else {
+							elem.removeClass(className);
+						}
+				};
+			},
+			link : function(scope, element){
+				
+				scope.addClass(element, className);		
+		}
+	};
+	})
+	.directive('slideButton', function(){
+		return{
+			require: "slideContent",
+			link: function(scope, element, attrs, slideContentCtrl){
+				var className = 'slidecontent';
+				element.on('click', function(){
+					slideContentCtrl.addClass(element, className);
+
+					console.log(element);
+				});
+			}
+		};
+	})*/
 	.controller('MainMenuController', ['$scope', 'navServices', function(scope, navServices){
-		
-		var scope.success = false,
-			scope.error = false;
 
 		var promise = navServices.getNavPromise();
 		promise.then(function(data){
-			
 			scope.menus = data.data;
-
-			setTimeout(function(){
-				alert('Hi I load after 5 seconds. I came in the middle of page to show you my progress');
-			}, 2000);
+			scope.success = true;
 		});
-		scope.hideNav = function(){
-			console.log('you clicked hideNav');
-			scope.status = 0;
-		};	
+
+		/*setTimeout(function(){
+			var notificationPromise = navServices.getNotification();
+			promise.then(function(){
+				//console.log("success");
+				scope.success = true;
+			});
+		},2000);*/
 	}])
 	.controller('MobileMenuController', ['$scope', function(scope){	
 		scope.navSlider = function(){
-			scope.status = !scope.status;			
+			var status = 0;
+
+			scope.status = !scope.status;
+			//console.log(slideContent.addClass());			
 		};
 	}]);
 	return navApp;
